5e6c65e61f5e4323ef3e37b7beaefc462dd59853
pedro geovanny <pemadev@hotmail.com>
2023-09-22 21:12:20 -0600

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,46 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortfolioComponent } from './components/pages/portfolio/portfolio.component';
+import { AboutComponent } from './components/portfolio/about/about.component';
+import { WorkComponent } from './components/portfolio/work/work.component';
+import { ArtComponent } from './components/portfolio/art/art.component';
+import { ProjectsComponent } from './components/portfolio/projects/projects.component';
+import { LinktreeComponent } from './components/pages/linktree/linktree.component';
+import { WelcomeComponent } from './components/portfolio/welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    component: PortfolioComponent,
+    children: 
+    [
+      {
+        path: '',
+        component: WelcomeComponent
+      },
+      {
+        path: 'about', 
+        component: AboutComponent
+      },
+      {
+        path: 'work', 
+        component: WorkComponent
+      },
+      {
+        path: 'art', 
+        component: ArtComponent
+      },
+      {
+        path: 'projects', 
+        component: ProjectsComponent
+      },
+    ]
+  },
+  {
+    path: 'linktree',
+    component: LinktreeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BackgroundComponent } from './components/globals/background/background.component';
+import { DarkmodeDirective } from './directives/darkmode.directive';
+import { PortfolioComponent } from './components/pages/portfolio/portfolio.component';
+import { LinktreeComponent } from './components/pages/linktree/linktree.component';
+import { AboutComponent } from './components/portfolio/about/about.component';
+import { ArtComponent } from './components/portfolio/art/art.component';
+import { ProjectsComponent } from './components/portfolio/projects/projects.component';
+import { WorkComponent } from './components/portfolio/work/work.component';
+import { NavComponent } from './components/portfolio/structure/nav/nav.component';
+import { WelcomeComponent } from './components/portfolio/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundComponent,
+    DarkmodeDirective,
+    PortfolioComponent,
+    LinktreeComponent,
+    AboutComponent,
+    ArtComponent,
+    ProjectsComponent,
+    WorkComponent,
+    NavComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
